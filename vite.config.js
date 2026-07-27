dbc9b468df63ec51ad90d70d14bf210aae28320a
Dominik Stempel <dominik.stempel7@gmail.com>
2026-07-27 11:48:01 +0200

    remove github pages

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/oi-list/",
+  base: "/",
   plugins: [react()],
   build: {
     rollupOptions: {
